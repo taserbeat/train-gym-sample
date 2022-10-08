78544fbbd486e60b2fb504232f00fef4121eec73
taserbeat <taserbeat@gmail.com>
2022-10-08 16:41:36 +0900

    fix: 宇宙船のコードを警告が出ないように修正

--- examples/spaceship.py
+++ examples/spaceship.py
@@ -2,6 +2,7 @@ import pygame
 import math
 import numpy as np
 import gym
+from gym import spaces
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.optimizers import Adam
@@ -10,6 +11,7 @@ from rl.policy import BoltzmannQPolicy
 from rl.agents.dqn import DQNAgent
 import os
 import pathlib
+
 
 # 宇宙船ゲームの強化学習サンプルコード
 # 以下の記事を参考にさせて頂いた
@@ -40,27 +42,25 @@ class game(gym.Env):
     def __init__(self):
         self.screen = pygame.display.set_mode((400, 300))
         self.clock = pygame.time.Clock()
-        self.action_space = gym.spaces.Discrete(2)
-        self.observation_space = gym.spaces.Box(low=np.float32([0, 0, 0]), high=np.float32([400, 300, math.pi]))
+        self.action_space = spaces.Discrete(2)
+        self.observation_space = spaces.Box(low=np.float32([0, 0, 0]), high=np.float32([400, 300, math.pi]))  # type: ignore
         self.reward_range = (-1, 1)
         self.logcount = 9999
 
     def reset(self):
-        self.observation = np.float32([
-            make_rnd(0, 100), make_rnd(100, 200), make_rnd(math.pi * 2 / 5, math.pi * 3 / 5)
-        ])
+        self.obs = np.float32([make_rnd(0, 100), make_rnd(100, 200), make_rnd(math.pi * 2 / 5, math.pi * 3 / 5)])  # type: ignore
         if self.logcount >= 10:
             self.screen.fill((0, 0, 0))
             pygame.draw.circle(self.screen, (100, 255, 255), (300, 150), 20)
             pygame.display.update()
             self.logcount = 0
         self.logcount += 1
-        return self.observation
+        return self.obs
 
     def render(self, mode):
-        x = self.observation[0] + 0.5
-        y = self.observation[1] + 0.5
-        r = self.observation[2]
+        x = self.obs[0] + 0.5  # type: ignore
+        y = self.obs[1] + 0.5  # type: ignore
+        r = self.obs[2]  # type: ignore
         pygame.draw.polygon(self.screen, (255, 255, 255), ((int(x), int(y)), (int(x + shipsin(r, -1)),
                             int(y - shipcos(r, -1))), (int(x + shipsin(r, 1)), int(y - shipcos(r, 1)))))
         pygame.display.update()
@@ -68,14 +68,18 @@ class game(gym.Env):
         return
 
     def step(self, act):
-        self.observation[2] += np.float(act * 2 - 1) / 20
-        self.observation[0] += math.sin(self.observation[2]) * 6
-        self.observation[1] -= math.cos(self.observation[2]) * 6
-        if self.observation[0] >= 400 or self.observation[1] <= 0 or self.observation[1] >= 300 or self.observation[2] <= 0 or self.observation[2] >= math.pi:
-            return self.observation, np.float(-1), True, {}  # 失敗（画面外に出た、船が真上か真下を向いた）、reward=-1
-        if (self.observation[0] - 300)**2 + (self.observation[1] - 150)**2 <= 400:
-            return self.observation, np.float(1), True, {}  # 成功（船が地球に到着した、地球の半径20)、reward=1
-        return self.observation, np.float(0), False, {}  # まだ飛行中、reward=0
+        self.obs[2] += np.float(act * 2 - 1) / 20  # type: ignore
+        self.obs[0] += math.sin(self.obs[2]) * 6  # type: ignore
+        self.obs[1] -= math.cos(self.obs[2]) * 6  # type: ignore
+        if self.obs[0] >= 400 or self.obs[1] <= 0 or self.obs[1] >= 300 or self.obs[2] <= 0 or self.obs[2] >= math.pi:  # type: ignore
+            # 失敗（画面外に出た、船が真上か真下を向いた）、reward=-1
+            return self.obs, np.float(-1), True, {}
+        if (self.obs[0] - 300)**2 + (self.obs[1] - 150)**2 <= 400:  # type: ignore
+            # 成功（船が地球に到着した、地球の半径20)、reward=1
+            return self.obs, np.float(1), True, {}
+
+        # まだ飛行中、reward=0
+        return self.obs, np.float(0), False, {}
 
 
 pygame.init()
@@ -98,11 +102,11 @@ memory = SequentialMemory(limit=50000, window_length=1)
 policy = BoltzmannQPolicy()
 dqn = DQNAgent(model=model, nb_actions=2, gamma=0.99, memory=memory, nb_steps_warmup=100, target_model_update=1e-2, policy=policy)
 dqn.compile(Adam(lr=1e-3), metrics=['mae'])
-dqn.fit(env, nb_steps=1000, visualize=True, verbose=1)  # visualize=Falseにすれば、画面描写をしなくなる
+dqn.fit(env, nb_steps=10000, visualize=True, verbose=1)  # visualize=Falseにすれば、画面描写をしなくなる
 
 if not os.path.exists(MODEL_DIR_PATH):
     os.makedirs(MODEL_DIR_PATH, exist_ok=True)
 
-dqn.model.save(MODEl_FILE_PATH, overwrite=True)
+dqn.model.save(MODEl_FILE_PATH, overwrite=True)  # type: ignore
 dqn.test(env, nb_episodes=10, visualize=True)
 pygame.quit()
