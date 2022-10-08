6a7296a8fe0dbfcd52505a5820e4fcff8a5675de
taserbeat <taserbeat@gmail.com>
2022-10-08 15:06:36 +0900

    feat: ライブラリのimportを動作確認するコードを追加

---
+++ examples/check_import.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import keras
+import rl
+import gym
+import pygame
+
+
+print(f"tf.__version__: {tf.__version__}")
+print(f"keras.__version__: {keras.__version__}")
+print(f"rl.__name__: {rl.__name__}")
+print(f"gym.__version__: {gym.__version__}")
+print(f"pygame.__name__: {pygame.__name__}")
